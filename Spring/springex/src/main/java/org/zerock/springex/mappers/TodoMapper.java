c1806528ac418b186c6239fc1753902ea5fa4739
DESKTOP-UJAB8N2\it <alswns3452@naver.com>
2025-12-11 16:37:03 +0900

    2025-12-11 수업내용

--- Spring/springex/src/main/java/org/zerock/springex/mappers/TodoMapper.java
+++ Spring/springex/src/main/java/org/zerock/springex/mappers/TodoMapper.java
@@ -1,8 +1,18 @@
 package org.zerock.springex.mappers;
 
+import org.zerock.springex.dto.PageRequestDTO;
+import org.zerock.springex.dto.TodoDTO;
 import org.zerock.springex.vo.TodoVO;
+
+import java.util.List;
 
 public interface TodoMapper {
     String getTime();
     void insert(TodoVO vo);
+    List<TodoVO> selectAll();
+    TodoVO selectById(Long tno);
+    void deleteById(Long tno);
+    void updateById(TodoVO vo);
+    List<TodoDTO> selectSearch(PageRequestDTO pageRequestDTO);
+    int getCount(PageRequestDTO pageRequestDTO);
 }
