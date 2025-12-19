28821bc0a0fb62bbe59c27c69582e1b845f96e75
DESKTOP-UJAB8N2\it <alswns3452@naver.com>
2025-12-19 16:31:53 +0900

    2025-12-19 수업 내용

--- SpringBoot/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
+++ SpringBoot/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
@@ -5,7 +5,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -18,8 +24,18 @@ public class Article {
     private String title;
     @Column(name="content", nullable = false)
     private String content;
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
     @Builder
     public Article(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+    public void update(String title, String content){
         this.title = title;
         this.content = content;
     }
---
+++ SpringBoot/springboot-developer/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (title, content, created_at, updated_at) values ('제목 1', '내용 1', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) values ('제목 1', '내용 1', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) values ('제목 1', '내용 1', NOW(), NOW())
