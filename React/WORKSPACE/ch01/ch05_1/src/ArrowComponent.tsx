a465c9d0ef4f2ac17410e3a8c1aa5be1a0712c90
maando <alswns3452@naver.com>
2026-01-29 16:36:43 +0900

    2026 01-29 리액트 수업내용

---
+++ React/WORKSPACE/ch01/ch05_1/src/ArrowComponent.tsx
@@ -0,0 +1,36 @@
+// import type { FC } from "react";
+// export type ArrowComponentProps = {
+//   href: string;
+//   text: string;
+// };
+// const ArrowComponent: FC<ArrowComponentProps> = (props) => {
+// const {href, text} = props;
+// return (
+//     <li>
+//       <a href={href}>
+//         <p>{text}</p>
+//       </a>
+//     </li>
+//   );
+// };
+
+// export default ArrowComponent;
+
+import type { ReactNode } from "react";
+
+interface DataProps {
+  href: string;
+  text: string;
+  children: ReactNode;
+}
+const ArrowComponent = ({ href, text, children }: DataProps) => {
+  return (
+    <li>
+      <a href={href}>
+        <p>{text}</p>
+      </a>
+      {children}
+    </li>
+  );
+};
+export default ArrowComponent;
